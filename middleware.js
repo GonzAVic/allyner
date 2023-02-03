e5eefe17b08e238fdb9f4f8126d932f2c1455ee9
Victor Gonzalez <victorgonz96@gmail.com>
2023-02-02 20:25:23 -0600

    444

--- middleware.js
+++ middleware.js
@@ -4,15 +4,15 @@ export default function middleware(req) {
   const { pathname } = req.nextUrl;
   const hostname = req.headers.get("host");
 
-  console.log("-> pathname: ", pathname);
-  console.log("-> hostname: ", hostname);
+  // console.log("-> pathname: ", pathname);
+  // console.log("-> hostname: ", hostname);
 
   const currentHost =
     process.env.NODE_ENV === "production"
       ? hostname?.replace(`.allyner.vercel.app`, "")
       : hostname?.replace(`.localhost:3001`, "");
 
-  console.log("-> currentHost: ", currentHost);
+  // console.log("-> currentHost: ", currentHost);
 
   if (pathname.startsWith(`/_sites`)) {
     return new Response(null, { status: 404 });
@@ -20,14 +20,7 @@ export default function middleware(req) {
 
   if (!pathname.includes(".") && !pathname.startsWith("/api")) {
     // For clients
-    if (currentHost === "app") {
-      const nextUrl = req.nextUrl.clone();
-      const newPathname = `/app${pathname}`;
-      nextUrl.pathname = newPathname;
-      return NextResponse.rewrite(nextUrl);
-    }
-
-    if (currentHost === "localhost:3000") {
+    if (pathname.startsWith("/app")) {
       const nextUrl = req.nextUrl.clone();
       const newPathname = `${pathname}`;
       nextUrl.pathname = newPathname;
