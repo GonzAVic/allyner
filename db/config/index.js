ae100ed3bafa07369add89e5cc24d132d7e82bbf
Victor Gonzalez <victorgonz96@gmail.com>
2023-06-17 17:04:02 -0600

    not sure what I am doing

--- db/config/index.js
+++ db/config/index.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const mongoDB = process.env.MONGO_DB_URI;
+const mongoDB = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
