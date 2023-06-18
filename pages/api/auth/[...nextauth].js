294494199761d2a83a73974371cf28471e672eaa
Victor Gonzalez <victorgonz96@gmail.com>
2023-06-18 13:41:09 -0600

    not asd asd ads

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+import connectDb from "db/config/index";
+import User from "db/models/User.model";
+import Business from "db/models/Business.model";
+
 console.log("-> LALALALALALALALALALALALALALA");
 
 export const authOptions = {
@@ -11,13 +15,47 @@ export const authOptions = {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
-      async authorize() {
-        return {
-          id: 1,
-          name: "J Smith",
-          email: "jsmith@example.com",
-          image: "https://i.pravatar.cc/150?u=jsmith@example.com",
-        };
+      async authorize(cred, req) {
+        try {
+          const signupData = JSON.parse(cred.email);
+          const { userData, businessData } = signupData;
+          await connectDb();
+
+          let user = await User.findOne({ email: userData.email });
+
+          // REQUESTER IS A CLIENT USER
+          if (userData.userType === "CLIENT") {
+            console.log("-> userData: ", userData);
+            user = await new User(userData);
+            console.log("-> user: ", user);
+            user.save();
+
+            // REQUESTER IS A BUSINESS USER
+          } else if (userData.userType === "BUSINESS") {
+            const business = await new Business(businessData);
+            business.save();
+            user = await new User({ ...userData, businessId: business.id });
+            user.save();
+          }
+
+          if (user) {
+            console.log("-> user: ", user);
+            const userData = {
+              email: user.email,
+              name: user.firstname,
+              id: user.id,
+            };
+            return userData;
+          } else {
+            console.log("-> no user");
+            // If you return null then an error will be displayed advising the user to check their details.
+            return null;
+
+            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          }
+        } catch (error) {
+          console.log("-> error: ", error);
+        }
       },
     }),
   ],
